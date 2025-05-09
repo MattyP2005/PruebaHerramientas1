89134028af88f2b6acff8bac8958fb77fb20e5d0
MattyP2005 <matias1976.perez@gmail.com>
2025-05-09 15:45:51 -0500

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,7 @@
+﻿SELECT * FROM Eventos;
+SELECT * FROM EventoPonente;
+SELECT * FROM Inscripciones;
+SELECT * FROM Pagos;
+SELECT * FROM Participantes;
+SELECT * FROM Ponentes;
+SELECT * FROM Sesiones;
